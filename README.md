f8a0ef06fde9bf0e0d80d047bd61811b19a58455
Lubov Vasilkova <damirana_@mail.ru>
2022-04-28 01:08:27 +0700

    project created

---
+++ README.md
@@ -0,0 +1,8 @@
+# CRUD
+
+Выполнена задача:
+
+[Склады и товары](./stocks_products)
+
+Работа соответствоваует принятому [стилю оформления кода](https://github.com/netology-code/codestyle/tree/master/python).
+
---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from .models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['quantity', 'price', 'product']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        print(positions)
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.get_or_create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+
+        return stock
