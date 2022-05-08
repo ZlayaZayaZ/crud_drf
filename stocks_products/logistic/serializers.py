ac3ad3aa5432304a64985e9dff4a3348564de309
Lubov Vasilkova <damirana_@mail.ru>
2022-05-08 09:52:17 +0700

    'Handling Does Not Exist exception'

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,4 +1,3 @@
-from requests import Response
 from rest_framework import serializers
 from .models import Product, Stock, StockProduct
 
@@ -46,9 +45,17 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем данные таблицы StockProduct
         # с помощью списка positions
         for position in positions:
-            stock_product = StockProduct.objects.get(stock=stock, product=position.get('product'))
-            stock_product.price = position.get('price', stock_product.price)
-            stock_product.quantity = position.get('quantity', stock_product.quantity)
-            stock_product.save()
-        return stock
 
+            try:
+                # ищем объект в базе данных StockProduct по id склада и продукта
+                # меняем цену и количество, на заданные в запросе, если объект найден
+                stock_product = StockProduct.objects.get(stock=stock, product=position.get('product'))
+                stock_product.price = position.get('price', stock_product.price)
+                stock_product.quantity = position.get('quantity', stock_product.quantity)
+                stock_product.save()
+
+            # если объект в базе не найден - ловим исключение и создаем объект
+            except StockProduct.DoesNotExist:
+                StockProduct.objects.create(stock=stock, **position)
+
+        return stock
